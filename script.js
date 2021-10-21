c8caef9858442637104817fdf49d29be91539429
Eduardo Aguiar <edu.pessoal@outlook.com>
2021-10-21 16:04:35 -0300

    verifica se a rede social é o youtube se for muda o link

--- script.js
+++ script.js
@@ -10,9 +10,13 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute("class");
 
-    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
+    if (social === "youtube") {
+      li.children[0].href = `https://${social}.com/channel/${linksSocialMedia[social]}`;
+    } else {
+      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
+    }
 
-    alert(li.children[0].href);
+    // alert(li.children[0].href);
   }
 }
 
