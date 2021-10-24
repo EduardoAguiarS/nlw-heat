15fbbf9af02af89cbcbd07a8c6b0dfc18a88d782
Eduardo Aguiar <edu.pessoal@outlook.com>
2021-10-23 23:16:58 -0300

    correçao do nome do objeto

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ createBtn.addEventListener("click", event => {
   function changeSocialMediaLinks() {
     for (let li of socialLinks.children) {
       const social = li.getAttribute("class");
-      li.children[0].href = `https://${social}.com/${linksSocialMedi[social]}`;
+      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
     }
   }
 
